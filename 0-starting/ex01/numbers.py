be480fa83bc70d3b5a02d7f076df7bce05c77f0a
agvangrigoryan <agvangrigoryan2003@gmail.com>
2025-06-12 20:44:30 +0400

    ex01 done

--- 0-starting/ex01/numbers.py
+++ 0-starting/ex01/numbers.py
@@ -1,7 +1,11 @@
 
-def print_splited_content():
+def print_splited_content(filename):
     with open(filename, "r") as file:
-        
+        line = file.readline()
+        if not line:
+            return
+        for digit in line.split(','):
+            print(digit)
 
 if __name__ == "__main__":
     filename = "numbers.txt"
