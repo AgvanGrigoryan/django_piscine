7db368226ee8e8757125e72fea556fbaaeb8e903
agvangrigoryan <agvangrigoryan2003@gmail.com>
2025-07-15 20:59:44 +0400

    ex04 done

---
+++ 1-lib/ex04/my_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+python3.12 -m venv django_venv
+
+django_venv/bin/pip install --upgrade pip setuptools wheel
+django_venv/bin/pip install -r requirement.txt --only-binary :all:
+
+source ./django_venv/bin/activate
+
+echo "Virtual environment 'django_venv' is ready."
+echo "Activating..."
+source ./django_venv/bin/activate
+$SHELL
