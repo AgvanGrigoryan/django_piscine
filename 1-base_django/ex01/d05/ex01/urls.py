7848ada80cdcfa588996c1b59078593ca28ae90c
agvangrigoryan <agvangrigoryan2003@gmail.com>
2025-07-19 14:16:13 +0400

    makes href linked by url names

--- 1-base_django/ex01/d05/ex01/urls.py
+++ 1-base_django/ex01/d05/ex01/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('django/', views.Render_ex01_django),
-    path('display/', views.Render_ex01_display),
-    path('templates/', views.Render_ex01_templates),
+    path('django/', views.Render_ex01_django, name="django"),
+    path('display/', views.Render_ex01_display, name="display"),
+    path('templates/', views.Render_ex01_templates, name="templates"),
 ]
