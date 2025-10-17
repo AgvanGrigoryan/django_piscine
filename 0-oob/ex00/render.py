1dcc3273f61a50dffe0d2de65faafd05ee125970
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2025-10-17 19:12:18 +0400

    fixed ex00

--- 0-oob/ex00/render.py
+++ 0-oob/ex00/render.py
@@ -1,9 +1,16 @@
 import os
 import sys
+import re
 
-def is_valid_file_ext(filename: str, expected_ext: str) -> bool:
+def is_valid_file(filename: str, expected_ext: str) -> bool:
     """check file's extension"""
-    return os.path.splitext(filename)[1] == expected_ext
+    if not os.path.exists(filename):
+        print("File not found", file=sys.stderr)
+        return False
+    if os.path.splitext(filename)[1] != expected_ext:
+        print("Wrong file extension", file=sys.stderr)
+        return False
+    return True
 
 def get_output_filename(filename: str) -> str:
     """generate the output filename based on input filename"""
@@ -13,19 +20,29 @@ def get_output_filename(filename: str) -> str:
 def get_context(settings_filename: str) -> dict:
     """Executes the settings file and returns its variables as a dictionary."""
 
-    namespace: dict = {}
+    context: dict = {}
     try:
-        with open(settings_filename, "r", encoding="utf-8") as f:
-            code = f.read()
-            exec(code, namespace)
-        return namespace
-
+        with open(settings_filename, "r", encoding="utf-8") as file:
+            content = file.read()
     except FileNotFoundError as e:
-        raise SystemExit("Settings file not found") from e
+        raise SystemExit(f"Settings file not found, expected {settings_filename}") from e
     except PermissionError as e:
         raise SystemExit("Permission denied while reading settings") from e
     except Exception as e:
         raise SystemExit("Unexpected error in settings file") from e
+
+    pattern = r'^([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*(.+)$'
+
+    for line in content.splitlines():
+        match = re.match(pattern, line)
+        if match:
+            name, value_str = match.groups()
+        try:
+            value = eval(value_str, {"__builtins__": None}, {})
+        except:
+            value = value_str.strip()
+        context[name] = value
+    return context
 
 def render_line(line: str, context: dict) -> str:
     """substitutes values from the context into the corresponding fields of the string"""
@@ -52,10 +69,10 @@ def process_template_file(input_filename: str, output_filename: str, context: di
 if __name__ == "__main__":
     if (
         len(sys.argv) != 2 
-        or not is_valid_file_ext(sys.argv[1], ".template")
+        or not is_valid_file(sys.argv[1], ".template")
     ):
         raise SystemExit("Invalid arguments, expected <filename>.template")
-    SETTINGS = "settings.py"
-    context: dict[str, str] = get_context(SETTINGS)
+    SETTINGS_FILE = "settings.py"
+    context: dict[str, str] = get_context(SETTINGS_FILE)
     process_template_file(sys.argv[1], get_output_filename(sys.argv[1]), context)
     
