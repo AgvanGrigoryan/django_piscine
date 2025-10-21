7394c8786c93d25e62da06a31710532729c358f5
Agvan Grigoryan <aggrigor@c2r1s6.42yerevan.am>
2025-10-21 15:40:17 +0400

    minor updates

--- 1-lib/ex01/my_script.sh
+++ 1-lib/ex01/my_script.sh
@@ -6,13 +6,13 @@ green="\e[32m"
 red="\e[31m"
 reset_color="\e[0m"
 
-echo -e $green"Using pip version:"$reset_color
+echo -en $green"Using pip version: "$reset_color
 pip --version
 
 mkdir -p $lib_folder
 
 echo -e $green"Installing path.py in ./$lib_folder/"$reset_color
-pip install --target $lib_folder --upgrade git+https://github.com/jaraco/path.git > $log_file 2>&1
+pip install --target $lib_folder --upgrade git+https://github.com/jaraco/path.py.git > $log_file 2>&1
 
 if [ $? -eq 0 ]; then
 	echo -e $green"Installation successful. Running Python script..."$reset_color
